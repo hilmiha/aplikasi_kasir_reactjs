b2f185f516167bd58ad14e43123d5e3811b132ca
Hilmi Hidayat Arfisko <hilmihidayatar@gmail.com>
2022-08-25 14:03:07 +0700

    penambahan fitur umum kasir seperti menambah dan mengurangi item Food N Drink dari kerajang serta menyimpan keranjang kedalam riwayat transaksi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import MenuPage from './components/Pages/Menu/MenuPage';
+import HistoryPage from './components/Pages/History/HistoryPage';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<MenuPage />} />
+					<Route path="/history" element={<HistoryPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
